df3564cffa640d62bd58b05b46965eab05c3a03b
Kalid Mubarek <Kalidmubarek@gmail.com>
2025-02-14 15:39:03 +0300

    Added WelcomeMessage component and updated App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+    return (
+        <div>
+            <WelcomeMessage />
+        </div>
+    );
+}
+
+export default App;
 
 function App() {
   const [count, setCount] = useState(0)

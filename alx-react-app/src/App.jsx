f4662d33f972a08a292b69ac6d52e3829e54c8e1
Kalid Mubarek <Kalidmubarek@gmail.com>
2025-02-14 16:02:10 +0300

    Added Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
+
 import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {

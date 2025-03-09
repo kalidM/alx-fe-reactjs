928f00407df5e32c6ac03c1842648af84aa502d2
kalidM <118064615+kalidM@users.noreply.github.com>
2025-03-09 18:19:34 +0300

    Update Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,4 +1,6 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
 
 const Profile = () => {
     return (
@@ -8,6 +10,10 @@ const Profile = () => {
                 <Link to="details">Profile Details</Link> |{' '}
                 <Link to="settings">Profile Settings</Link>
             </nav>
+            <Routes>
+                <Route path="details" element={<ProfileDetails />} />
+                <Route path="settings" element={<ProfileSettings />} />
+            </Routes>
             <Outlet />
         </div>
     );
